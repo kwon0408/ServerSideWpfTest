4871dbf2571b53fdae336a1cd38c6d7dba635e38
J. Kwon <henry.kwon.0408@gmail.com>
2022-12-30 14:13:18 +0900

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServerSideWpfTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
